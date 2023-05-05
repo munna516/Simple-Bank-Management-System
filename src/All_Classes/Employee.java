884b9a5cdacc74e552384633fe7110ff3a2bdcd6
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2023-05-06 01:05:44 +0600

    employee & interfaces added

--- src/All_Classes/Employee.java
+++ src/All_Classes/Employee.java
@@ -1,5 +1,33 @@
 package All_Classes;
 
+import java.lang.*;
+
 public class Employee {
-    
+    private String emid;
+    private String emname;
+    private double emsalary;
+
+    public void SetEmployeeId(String id) {
+        this.emid = id;
+    }
+
+    public void SetEmployeeName(String name) {
+        this.emname = name;
+    }
+
+    public void SetEmployeeSalary(double salary) {
+        this.emsalary = salary;
+    }
+
+    public String getEmpid() {
+        return emid;
+    }
+
+    public String getEmpName() {
+        return emname;
+    }
+
+    public double getEmpSalary() {
+        return emsalary;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 
+import All_Classes.Employee;
+
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         boolean choice = true;
-        System.out.println("\n\nWELCOME TO SIMPLE BANK MANAGEMENT SYSTEM");
+        System.out.println("\n\nWELCOME TO SIMPLE BANK MANAGEMENT SYSTEM\n");
 
         while (choice) {
             System.out.println("Here Are Some Option-\n");
@@ -13,122 +15,165 @@ public class Main {
             System.out.println("     3. Customer Account Management");
             System.out.println("     4. Account Transactions");
             System.out.println("     5. Exit\n");
-            System.out.print("What do you want to do? : ");
+            System.out.print(">>> Enter a Option : ");
             int first = input.nextInt();
             switch (first) {
                 case 1:
-                    System.out.println("You Have Selected Employee Management.");
+                    System.out.println("You Have Selected Employee Management.\n");
                     System.out.println("Here are some option-\n");
                     System.out.println("     1. Create New Employee");
                     System.out.println("     2. Remove Employee");
                     System.out.println("     3. See All Employee");
                     System.out.println("     4. Go Back\n");
-                    System.out.print("What do you want to do? : ");
+                    System.out.print(">>> Enter a Option : ");
                     int second1 = input.nextInt();
                     switch (second1) {
                         case 1:
                             System.out.println("You Have Selected Create a New Employee\n");
                             System.out.print("Enter Employee Id : ");
                             String emid = input.nextLine();
+
                             String ch1 = input.nextLine();
+
                             System.out.print("Enter Employee Name : ");
                             String emname = input.nextLine();
+
                             System.out.print("Enter Employee Salary : ");
                             double emsalary = input.nextDouble();
+
                             String ch2 = input.nextLine();
-                            // Employee emp = new Employee();
+
+                            Employee emp = new Employee();
+                            emp.SetEmployeeId(emid);
+                            emp.SetEmployeeName(emname);
+                            emp.SetEmployeeSalary(emsalary);
+
+                            break;
+
                         case 2:
                             System.out.println("You Have Selected to Remove an Employee\n");
                             System.out.print("Enter Employee Id : ");
                             String id1 = input.nextLine();
 
                             break;
+
                         case 3:
-                            System.out.println("You Have Selected to See All Employee");
+                            System.out.println("You Have Selected to See All Employee\n");
+                            break;
+
+                        case 4:
+                            System.out.println("Going Back...");
+                            break;
+
+                        default:
+                            System.out.println("Invalid Input");
+                            break;
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("You Have Selected Customer Management\n");
+                    System.out.println("Here are some option-\n");
+                    System.out.println("     1. Create New Customer");
+                    System.out.println("     2. Remove Customer");
+                    System.out.println("     3. See All Customers");
+                    System.out.println("     4. Go Back\n");
+                    System.out.print(">>> Enter a Option : ");
+                    int second2 = input.nextInt();
+                    switch (second2) {
+                        case 1:
+                            System.out.println("You Have Selected Create a New Customer\n");
+                            System.out.print("Enter Customer NID : ");
+                            int nid1 = input.nextInt();
+
+                            String ch3 = input.nextLine();
+
+                            System.out.print("Enter Customer Name : ");
+                            String name1 = input.nextLine();
+                            break;
+                        case 2:
+                            System.out.println("You Have Selected to Remove a Customer\n");
+                            System.out.print("Enter Customer NID : ");
+                            int nid2 = input.nextInt();
+                            break;
+                        case 3:
+                            System.out.println("You Have Selected to See All Customer\n");
                             break;
                         case 4:
                             System.out.println("Going Back...");
                             break;
                         default:
                             System.out.println("Invalid Input");
-                            break;
-
-                    }
-                    break;
-
-                case 2:
-                    System.out.println("You Have Selected Customer Management");
-                    System.out.println("Here are some option-\n");
-                    System.out.println("     1. Create New Customer");
-                    System.out.println("     2. Remove Customer");
-                    System.out.println("     3. See All Customers");
-                    System.out.println("     3. Go Back\n");
-                    System.out.print("What do you want to do? : ");
-                    int second2 = input.nextInt();
-                    switch (second2) {
-                        case 1:
-                            System.out.println("You Have Selected Create a New Customer\n");
-                            System.out.print("Enter Customer NID : ");
-                            int nid = input.nextInt();
-                            String ch1 = input.nextLine();
-                            System.out.print("Enter Customer Name : ");
-                            String emname = input.nextLine();
-                        case 2:
-                            System.out.println("You Have Selected to Remove a Customer\n");
-                            System.out.print("Enter Customer NID : ");
-                            int nid1 = input.nextInt();
-                            break;
-                        case 3:
-                            System.out.println("You Have Selected to See All Customer");
-                            break;
-                        case 4:
-                            break;
-                        default:
                             break;
 
                     }
                     break;
 
                 case 3:
-                    System.out.println("You Have Selected Customer Account Management");
+                    System.out.println("You Have Selected Customer Account Management\n");
                     System.out.println("Here are some option-\n");
                     System.out.println("	1. Create New Account for a Customer");
                     System.out.println("	2. Remove Account of a Customer");
                     System.out.println("	3. See all Accounts of a Customer");
                     System.out.println("	4. Go Back\n");
-                    System.out.print("What do you want to do? : ");
+                    System.out.print(">>> Enter a Option : ");
                     int second3 = input.nextInt();
                     switch (second3) {
                         case 1:
-                            System.out.println("There are two types of Account");
+                            // Account a = NULL;
+
+                            System.out.println("There are two types of Account\n");
                             System.out.println("     1. Savings Account");
                             System.out.println("     2. Fixed Account");
                             System.out.println("     3. Go Back\n");
-                            System.out.print("What do you want to do? : ");
+                            System.out.print(">>> Enter a Option : ");
                             int actype = input.nextInt();
+                            // Generating Random Account Number
+                            int an1 = (int) (Math.random() * (9999 - 999 + 1) + 999);
+                            System.out.print("Enter Opening Balance: ");
+                            double ob1 = input.nextDouble();
+
+                            if (actype == 1) {
+                                System.out.print("Enter Interest Rate: ");
+                                double ir = input.nextDouble();
+                            } else if (actype == 2) {
+                                System.out.print("Enter Expired Year: ");
+                                int ex = input.nextInt();
+
+                            }
+
                             break;
+
                         case 2:
+                            System.out.println("You Have Selected to Remove Account of a Customer\n");
+                            System.out.print("Enter Customer Nid: ");
+                            int nid2 = input.nextInt();
+                            System.out.print("Enter Account Number: ");
+                            int an2 = input.nextInt();
                             break;
+
                         case 3:
-                            System.out.println("You Have Selected to See All Accounts of Customer");
+                            System.out.println("You Have Selected to See All Accounts of Customer\n");
                             break;
                         case 4:
+
                             System.out.println("Going Back...");
                             break;
+
                         default:
                             System.out.println("Invalid Input");
                             break;
                     }
                     break;
                 case 4:
-                    System.out.println("You have selected Account Transactions");
+                    System.out.println("You have selected Account Transactions\n");
                     System.out.println("Here are Some Options- \n");
                     System.out.println("     1. Deposit Money");
                     System.out.println("     2. Withdraw Money");
                     System.out.println("     3. Transfer Money");
                     System.out.println("     4. Show Transaction History");
                     System.out.println("     5. Go Back\n");
-                    System.out.print("What do you want to do? : ");
+                    System.out.print(">>> Enter a Option : ");
                     int second4 = input.nextInt();
                     switch (second4) {
                         case 1:
@@ -144,7 +189,7 @@ public class Main {
                     }
                 case 5:
                     System.out.println("\nYou Have Selected to Exit the Application");
-                    System.out.println("Thank You for Using Our Simple Bank......");
+                    System.out.println("Thank You for Using Our Simple Bank......\n");
                     choice = false;
                     break;
                 default:
